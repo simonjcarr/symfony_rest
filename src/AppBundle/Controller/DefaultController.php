8b891dcffb713ce546e7aabc63c8fc67b25b0f1f
Simon Carr <simonjcarr@gmail.com>
2017-10-26 21:33:46 +0100

    Initial commit

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -17,5 +18,27 @@ class DefaultController extends Controller
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
+    }
+
+    /**
+     * @Route("/api/test",name="api_test")
+     */
+    public function testAction(Request $request)
+    {
+        $data = [
+            'message'=>'Hello World'
+        ];
+        $json = json_encode($data);
+
+        $response = new Response($json, 200);
+
+
+//        $response->headers->set('Access-Control-Allow-Headers', 'origin, content-type, accept');
+//        $response->headers->set('Access-Control-Allow-Origin', '*');
+//        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH, OPTIONS');
+//        $response->headers->set('Access-Control-Request-Method', '*');
+
+        return $response;
+
     }
 }
